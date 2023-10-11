7a904d5efc5a6996c7a3c14c5f90c9ada1fd2f81
HalleluyahBoy <eejimone@gmail.com>
2023-10-11 07:11:13 +0100

    making some neccessary adjustments

--- RIdes/block/contracts/Modify.sol
+++ RIdes/block/contracts/Modify.sol
@@ -1,27 +1,33 @@
 // SPDX-License-Identifier: MIT
+// Contract for saving driver and passenger,
+// in this contract an account, it'll require an account, from both parties(driver and passenger)
+// the account will be funded with token and crypto
+// the contract will also be rewarding drivers initially for signing up and referral
+
+
 pragma solidity ^0.8.0;
 
-contract RIdes {
+contract RideHailingPlatform {
     address public owner;
-    
+
+    // Enum to represent the status of a ride
     enum RideStatus { Requested, Accepted, InProgress, Completed, Canceled }
 
+    // Struct to represent a ride
     struct Ride {
         address driver;
         address passenger;
         uint256 fare;
         RideStatus status;
-        string startLocation;
-        string endLocation;
-        uint256 startTime;
-        uint256 endTime;
     }
 
     Ride[] public rides;
-    
+
+    // Mapping to keep track of rides for each driver and passenger
     mapping(address => uint256[]) public driverRides;
     mapping(address => uint256[]) public passengerRides;
 
+    // Events to log important contract actions
     event RideRequested(address indexed passenger, uint256 indexed rideId);
     event RideAccepted(uint256 indexed rideId);
     event RideCompleted(uint256 indexed rideId);
@@ -31,19 +37,22 @@ contract RIdes {
         owner = msg.sender;
     }
 
+    // Modifier to restrict access to the contract owner
     modifier onlyOwner() {
         require(msg.sender == owner, "Only the contract owner can perform this action");
         _;
     }
 
-    function requestRide(string memory start, string memory end) external payable {
+    // Function for passengers to request a ride
+    function requestRide() external payable {
         require(msg.value > 0, "Payment is required to request a ride");
-        uint256 rideId = rides.length;
-        rides.push(Ride(msg.sender, address(0), msg.value, RideStatus.Requested, start, end, 0, 0));
+        rides.push(Ride(msg.sender, address(0), msg.value, RideStatus.Requested));
+        uint256 rideId = rides.length - 1;
         passengerRides[msg.sender].push(rideId);
         emit RideRequested(msg.sender, rideId);
     }
 
+    // Function for drivers to accept a ride request
     function acceptRide(uint256 _rideId) external {
         require(_rideId < rides.length, "Invalid ride ID");
         require(msg.sender != rides[_rideId].driver, "You cannot accept your own ride");
@@ -51,19 +60,18 @@ contract RIdes {
 
         rides[_rideId].driver = msg.sender;
         rides[_rideId].status = RideStatus.Accepted;
-        rides[_rideId].startTime = block.timestamp; // Record start time
         driverRides[msg.sender].push(_rideId);
 
         emit RideAccepted(_rideId);
     }
 
+    // Function for drivers to mark a ride as completed and get paid
     function completeRide(uint256 _rideId) external {
         require(_rideId < rides.length, "Invalid ride ID");
         require(msg.sender == rides[_rideId].driver, "Only the driver can complete the ride");
         require(rides[_rideId].status == RideStatus.InProgress, "Ride is not in progress");
 
         rides[_rideId].status = RideStatus.Completed;
-        rides[_rideId].endTime = block.timestamp; // Record end time
 
         // Calculate and transfer payment to the driver
         payable(msg.sender).transfer(rides[_rideId].fare);
@@ -71,6 +79,7 @@ contract RIdes {
         emit RideCompleted(_rideId);
     }
 
+    // Function for passengers to cancel a ride and get a refund
     function cancelRide(uint256 _rideId) external {
         require(_rideId < rides.length, "Invalid ride ID");
         require(msg.sender == rides[_rideId].passenger, "Only the passenger can cancel the ride");
@@ -84,8 +93,5 @@ contract RIdes {
         emit RideCanceled(_rideId);
     }
 
-    function getRideDetails(uint256 _rideId) external view returns (Ride memory) {
-        require(_rideId < rides.length, "Invalid ride ID");
-        return rides[_rideId];
-    }
+    // Add more functions for handling additional features like rating drivers, handling disputes, etc.
 }
